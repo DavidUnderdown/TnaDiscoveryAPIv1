b23b2f72ca2e7209af5a417845a877ea2856f6ee
RDavies2 <ron@unlimit.co.uk>
2019-02-27 12:55:27 +0000

    moved class definitions to module:x

--- AssetCollectionVMofInfoAssetIVM.py
+++
@@ -1,9 +0,0 @@
-class AssetCollectionViewModelOfInformationAssetIdentityViewModel:
-    _count = 0
-    def __init__(self):
-        _count +=1
-        self.Assets = []  # Collection of InformationAssetIdentityViewModel  INfoAssetIVM
-        self.HasMoreAfterLast = False
-        self.HasMoreBeforeFirst = False
-
-
--- CollectionOfInfoAssetIVM.py
+++
@@ -1,5 +0,0 @@
-class CollectionOfAssetIVM:
-    _count = 0
-    def __init__(self):
-        _count += 1
-
